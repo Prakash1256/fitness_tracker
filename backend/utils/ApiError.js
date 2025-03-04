88ccfc2713932be4e46f0dc0952b9dae51d5cf4d
prakash <ps4059269@gmail.com>
2025-03-04 23:14:29 +0530

    fix the utils

--- backend/utils/ApiError.js
+++ backend/utils/ApiError.js
@@ -1,22 +1,41 @@
-class ApiError extends Error {
-    constructor(
-        statusCode,
-        message = "Something went wrong",
-        errors = [],
-        stack = ""
-    ) {
-        super(message);
-        this.statusCode = statusCode;
-        this.data = null;
-        this.success = false;
-        this.errors = errors;
+// class ApiError extends Error {
+//     constructor(
+//         statusCode,
+//         message = "Something went wrong",
+//         errors = [],
+//         stack = ""
+//     ) {
+//         super(message);
+//         this.statusCode = statusCode;
+//         this.data = null;
+//         this.success = false;
+//         this.errors = errors;
 
-        if (stack) {
-            this.stack = stack;
-        } else {
-            Error.captureStackTrace(this, this.constructor);
-        }
+//         if (stack) {
+//             this.stack = stack;
+//         } else {
+//             Error.captureStackTrace(this, this.constructor);
+//         }
+//     }
+// }
+
+// export default ApiError;
+
+
+function ApiError(statusCode, message = "Something went wrong", errors = [], stack = "") {
+    const error = new Error(message);
+    error.statusCode = statusCode;
+    error.data = null;
+    error.success = false;
+    error.errors = errors;
+
+    if (stack) {
+        error.stack = stack;
+    } else {
+        Error.captureStackTrace(error, ApiError);
     }
+
+    return error;
 }
 
 export default ApiError;
